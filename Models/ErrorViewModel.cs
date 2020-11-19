11a1ddbfa78c275742819dbf12f2046f3fa5ed69
KhibaIC <itumelengcalvin7@gmail.com>
2020-11-19 21:08:28 +0200

    commit yaka

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CMPG323Project2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
